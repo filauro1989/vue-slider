5878c98673ae16d1cca68b67183cd0f612f50dac
filauro1989 <filauro1989@gmail.com>
2021-12-16 15:09:21 +0100

    next prev

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Descrizione:
+// Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.
+// Bonus:
+// 1- al click su uno dei pallini mostrare l’immagine in posizione corrispondente
+// 2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            counter: 0,
+            images: [
+                'image1.jpg',
+                'image2.jpg',
+                'image3.jpg',
+                'image4.jpg'
+            ]
+        },
+        methods: {
+            next: function () {
+                this.counter += 1;
+                if (this.counter > this.images.length - 1) {
+                    this.counter = 0;
+                }
+            },
+            prev: function () {
+                this.counter -= 1;
+                if (this.counter < 0) {
+                    this.counter = this.images.length - 1;
+                }
+            }
+        }
+    }
+);
